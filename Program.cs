5fd3f69ebe3d84549d5314a3b2b584f5c6f754d3
Oleg <olmag@inbox.ru>
2023-02-05 17:44:24 +0300

    task50

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции
+// элемента в двумерном массиве, и возвращает значение этого элемента
+// или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+int[,] FillMatrix(int rows, int cols)
+{
+    Random rand = new Random();
+    int[,] matr = new int[rows, cols];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matr[i, j] = rand.Next(0, 10);
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write(matr[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+bool SearchForElement(int[,] matr)
+{
+    System.Console.WriteLine("Введите элемент для поиска: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (matr[i, j] == num)
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+System.Console.WriteLine("Введите m и n: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = FillMatrix(m, n);
+
+System.Console.WriteLine();
+
+PrintMatrix(matrix);
+System.Console.WriteLine();
+
+if (SearchForElement(matrix))
+{
+    System.Console.WriteLine("Элемент содержится в массиве");
+}
+else
+{
+    System.Console.WriteLine("Элемент не содержится в массиве");
+}
